2723d40d95f5d8dd86078a056af6b5e3bd042b1b
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-11-08 08:45:43 +0100

    Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace prijestupneGodine
 
             for (int i = 1900; i <= 2100; i++)
             {
-                if (i%400==0 && i%100 !=0 && i%4==0)
+                if (i%400==0 || i%100 !=0 && i%4==0)
                 {
                     Console.WriteLine(i);
                 }
