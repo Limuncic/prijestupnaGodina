3acf9ac663607d7ea1308c7796ab8e8aec076420
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-11-08 08:44:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prijestupneGodine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sve prijestupne godine 1900 - 2100: ");
+
+            for (int i = 1900; i <= 2100; i++)
+            {
+                if (i%400==0 && i%100 !=0 && i%4==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
